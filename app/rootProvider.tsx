66dd0e4850872893934cc42ce0bf7bb4f7c485da
Kilian Valdman <kilianvaldman@gmail.com>
2025-10-19 00:41:33 +0200

    base mainnet v1

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ReactNode } from "react";
-import { baseSepolia } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import "@coinbase/onchainkit/styles.css";
 
@@ -8,7 +8,7 @@ export function RootProvider({ children }: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={baseSepolia}
+      chain={base}
       config={{
         appearance: {
           mode: "auto",
